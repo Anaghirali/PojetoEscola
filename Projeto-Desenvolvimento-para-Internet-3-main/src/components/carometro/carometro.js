53131b8696deacba5ad5621e3c3e1ae67948d2d7
Ana <anaghirali@gmail.com>
2022-10-27 00:52:17 -0300

    commit

--- Projeto-Desenvolvimento-para-Internet-3-main/src/components/carometro/carometro.js
+++ Projeto-Desenvolvimento-para-Internet-3-main/src/components/carometro/carometro.js
@@ -1,48 +1,55 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-
+//import './Carometro.css';
 
 const Carometro = () => {
-
     const urlApiAlunos = "http://localhost:5035/api/controller"
     const urlAPICursos = "http://localhost:5035/api/controller/cursos"
-    const [dataAtualizada, setDataAtualizada] = useState(true)
 
+   
+    const [dataAtualizada, setDataAtualizada] = useState(true)
     const initialState = {
-        aluno: {id: 0, ra: '', nome: '', codCurso: 0},
+        aluno: { id: 0, ra: '', nome: '', codCurso: 0 },
         curso: { id: 0, codCurso: "", nomeCurso: "", periodo: "" },
         listaAlunos: [],
         listaCursos: [],
     }
 
-    const [listaCursos, setListaCursos] = useState(initialState.listaCursos);
+    
     const [listaAlunos, setListaAlunos] = useState(initialState.listaAlunos);
     const [curso, setCurso] = useState(initialState.curso);
 
-    const avatar = ['pixel-art']
+    const [vetCurso, setVetCurso] = useState([])
+    const [vetAluno, setVetAluno] = useState([])
 
+    const [dadosCurso, setDadosCurso] = useState({
+        curso: { id: 0, codCurso: "", nomeCurso: "", periodo: "" },
+    })
+    const [dadosAluno, setDadosAlunos] = useState({
+        aluno: { id: 0, ra: '', nome: '', codCurso: 0 }
+    })
+
+    const avatar = ['adventurer']
 
     function ramdomAvatar() {
-        let tamanho = avatar.length
-        let av = Math.floor(Math.random() * tamanho)
-        let rd = avatar[av]
-        return rd
-    }
-    const RandomLetter = () => {
-        return Math.random().toString(36).substring(2, 9);
+        let t = avatar.length
+        let av = Math.floor(Math.random() * t)
+        let random = avatar[av]
+        return random
     }
 
-    function randomString(tamanho) {
-        let stringAleatoria = '';
-        let caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    function randomString(t) {
+       return Math.random().toString(36).substring(1, 9)
+    }
 
+    let imgURL = () => `https://avatars.dicebear.com/api/${ramdomAvatar()}/${randomString(8)}.svg`
 
     const dataFromAPI = async () => {
-        await axios(urlApiAlunos)
+
+            await axios(urlApiAlunos)
             .then(resp => {
-                setDataAtualizada(true)
-                setListaCursos(resp.data)
+               setDataAtualizada(true)
+               setVetCurso(resp.data)
             })
             .catch(error => {
                 console.error(error)
@@ -50,6 +57,7 @@ const Carometro = () => {
     }
 
     const dataFromAPI2 = async (codCurso) => {
+
         return await axios(urlApiAlunos)
             .then(resp => {
                 const DataCursos = resp.data
@@ -66,16 +74,13 @@ const Carometro = () => {
     const updateAlunos = async (e) => {
         const codCurso = e.target.value;
         if (e.target.value === "") {
-            setListaAlunos(initialState.listaAlunos);
-            setCurso(initialState.curso);
-            return
+            setVetAluno(dadosAluno.aluno);
+            setDadosCurso(dadosCurso.curso);
         }
-        curso.codCurso = Number(codCurso)
-        const listaDeAlunos = await dataFromAPI2(curso.codCurso)
-        if(!Array.isArray(listaDeAlunos)) return
-
-        setListaAlunos(listaDeAlunos)
-        setCurso(curso)
+        dadosCurso.codCurso = Number(codCurso)
+        const listaDeAlunos = await dataFromAPI2(dadosCurso.codCurso)
+        setVetAluno(listaDeAlunos)
+        setDadosCurso(curso)
     }
 
 
@@ -86,53 +91,52 @@ const Carometro = () => {
         }
     }, [dataAtualizada])
 
-
-    const SelectOPT = () => {
-        return (
-            <select name="codCurso" onChange={e => { updateAlunos(e) }}>
-                {listaCursos.map(
-                    (curso) =>
-                        <option
-                            key={curso.id}
-                            name="codCurso"
-                            value={curso.codCurso}
-                        >
-                            {curso.nomeCurso}
-                            -
-                            {curso.periodo}
-                        </option>
-
-                )}
-            </select>
-        )
-    }
-
     return (
-        <div className="text-center duration-75 rounded-lg">
-            <span>{SelectOPT()}</span>
-            <div className="flex flex-wrap gap-5 w-screen items-center justify-between py-10 px-60 rounded-lg">
-                {listaAlunos.map((datas) => {
-                    return (
-                        <div key={datas.id} className="flex flex-wrap p-5 shadow-2xl shadow-blue-700 h-[370px] rounded-lg">
-                            <div className="">
-                                <div className="">
-                                    <div className="w-12/12 items-center self-center place-items-center">
-                                        <img src={`https://avatars.dicebear.com/api/bottts/${RandomLetter()}.svg`} alt={datas.nome} />
-                                    </div>
-                                </div>
 
-                                <div className="flex flex-col flex-wrap w-[180px] pt-5 gap-3 text-left">
-                                    <span className="text-xl font-medium">{datas.nome}</span>
-                                    <span className="font-medium">RA: {datas.ra}</span>
-                                    <span className="">Curso: {datas.codCurso}</span>
+        
+        <div className="text-center duration-75 rounded-lg">
+            <div className="flex items-center justify-between pl-10 py-10">
+                <select name="codCurso" onChange={e => { updateAlunos(e) }}>
+                    <option name="initialValue" value="" selected>Escolha um Curso</option>
+                    {vetCurso.map(
+                        (curso) =>
+                            <option
+                                key={curso.id}
+                                name="codCurso"
+                                value={curso.codCurso}
+                            >
+                                {curso.nomeCurso}
+                                -
+                                {curso.periodo}
+                            </option>
+
+                    )}
+                </select>
+            </div>
+        <div className="flex flex-wrap  gap-5 w-screen items-center justify-between py-10 px-40 min-w-[100px]">
+            {vetAluno.map((datas) => {
+                return (
+                        <div key={datas.id} className="flex flex-wrap p-5 shadow-2xl shadow-blue-700 w-[200px] h-[350px]">
+                            <div className="">
+                            <div className="">
+                                <div className="w-9/12 items-center self-center place-items-center">
+                                    <img src={imgURL()} alt={datas.nome} className="" loading="lazy" />
                                 </div>
                             </div>
+
+                            <div className="flex flex-col flex-wrap w-[180px] pt-5 gap-3 text-left">
+                                <span className="text-teal-500 font-medium">{datas.nome}</span>
+                                <span className="font-medium">RA: {datas.ra}</span>
+                                <span className="">Curso: {datas.ra}</span>
+                                
+                            </div>
                         </div>
-                    )
-                })}
-            </div>
+                    </div>
+                )
+            })}
         </div>
-    )
- }
+    </div>
+)
 }
+
 export default Carometro
